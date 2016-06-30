da001faedb73595e63419e93acd74373d1bf3f34
cod7alex <cod7alex@gmail.com>
2016-06-30 21:56:03 +0300

    Added anti forgery token

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -16,6 +16,7 @@ namespace GigHub.Controllers
         }
 
         [Authorize]
+        [ValidateAntiForgeryToken]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
