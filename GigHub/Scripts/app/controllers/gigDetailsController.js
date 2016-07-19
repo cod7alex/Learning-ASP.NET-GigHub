9eb4d40d878b9de3adc0a799eefb3f0c734aca4d
cod7alex <cod7alex@gmail.com>
2016-07-19 21:25:52 +0300

    Refactoring: introduce javascript modules, added toggle for Going to gig

---
+++ GigHub/Scripts/app/controllers/gigDetailsController.js
@@ -0,0 +1,33 @@
+﻿var GigDetailsController = function (followingsService) {
+    var button;
+
+    var init = function (container) {
+        $(container).on("click", ".js-toggle-following", toggleFollowing);
+    };
+
+    var toggleFollowing = function (e) {
+        button = $(e.target);
+
+        var artistId = button.data("artist-id");
+
+        if ($(button).hasClass("btn-default"))
+            followingsService.addFollowing(artistId, done, fail);
+        else
+            followingsService.deleteFollowing(artistId, done, fail);
+    };
+
+    var done = function () {
+        var text = (button.text() == "Following") ? "Follow" : "Following";
+
+        button.toggleClass("btn-info").toggleClass("btn-default").text(text);
+    };
+
+    var fail = function() {
+        alert("Something failed");
+    };
+
+    return {
+        init: init
+    }
+
+}(FollowingsService);
---
+++ GigHub/Scripts/app/services/followingService.js
@@ -0,0 +1,21 @@
+﻿var FollowingsService = function () {
+    var addFollowing = function (artistId, done, fail) {
+        $.post("/api/following", { artistId: artistId })
+        .done(done)
+        .fail(fail);
+    };
+
+    var deleteFollowing = function (artistId, done, fail) {
+        $.ajax({
+            url: "/api/following/" + artistId,
+            method: "DELETE"
+        })
+        .done(done)
+        .fail(fail);
+    };
+
+    return {
+        addFollowing: addFollowing,
+        deleteFollowing: deleteFollowing
+    };
+}();
